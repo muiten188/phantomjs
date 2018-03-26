dde77ef59ecf93a0146745889571f83fd1d562b4
ISPCO\bachbd <buidinhbach123@gmail.com>
2018-03-26 11:30:22 +0700

    git push #1

--- checkId.js
+++ checkId.js
@@ -1,13 +1,20 @@
 var page = require('webpage').create();
 console.log("please waiting...")
-page.viewportSize = {
-  width: 1349,
-  height: 3000
-};
-page.settings.userAgent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.4.3202.15';
+// page.viewportSize = {
+//   width: 1349,
+//   height: 3000
+// };
+page.settings.userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1';
 page.onConsoleMessage = function(msg, lineNum, sourceId) {
   console.log(msg);
 };
+var fs = require('fs');
+
+var content = fs.read('file.txt');
+console.log('read data:', content);
+var path = 'output.txt';
+fs.write(path, content, 'w');
+
 page.open("https://reportaproblem.apple.com", function (status) {
 
   if (status === "success") {
@@ -15,12 +22,11 @@ page.open("https://reportaproblem.apple.com", function (status) {
       console.log('afunc: ' +msg);
       };
     window.setTimeout(function () {
-
-      page.render("checkID0.png");
       // sendKeys(page, '*[id=appleId]', "a0i21-patsy273@yahoo.com");
       // sendKeys(page, '*[id=pwd]', "Zxcv123123");
       sendKeys(page, '*[id=appleId]', "applevantue-439398669388@yahoo.com");
       sendKeys(page, '*[id=pwd]', "Tienhn123");
+      page.render("checkID0.png");
       page.evaluate(function () {
         // focus on the text element before typing
         var element = document.getElementById('aid-auth-widget-iFrame').contentWindow.document.getElementById('sign-in');
@@ -40,8 +46,8 @@ page.open("https://reportaproblem.apple.com", function (status) {
         });
 
         phantom.exit();
-      }, 2000);
-    }, 3000);
+      }, 5000);
+    }, 5000);
 
   }
   function sendKeys(page, selector, keys) {
